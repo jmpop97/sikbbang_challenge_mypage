0f782eded61b488091bf2ba297e251dd6dda9a4f
jmpop <pjm970128@gmail.com>
2023-04-12 15:36:24 +0900

    html추가

--- testapp/models.py
+++ testapp/models.py
@@ -6,3 +6,6 @@ class TestApp(models.Model):
         db_table="test_app"
     name = models.CharField(max_length=20)
     something = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
