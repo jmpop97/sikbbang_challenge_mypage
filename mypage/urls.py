d5c388c0989c663c198ff44c6e0770190544873d
jmpop <pjm970128@gmail.com>
2023-04-12 20:21:22 +0900

    mypage add,delete

--- mypage/urls.py
+++ mypage/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views   # user/views 불러오기
 
 urlpatterns = [
-    path('test/', views.mychallenge, name='a'),
+    path('test1/', views.mychallenge, name='a'),
+    path('test/', views.mychallengeadd, name='a'),
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -5,6 +5,13 @@ from testapp.models import TestApp
 def mychallenge(request):
     testkey1=MyPage.objects.get(id='5')
     print(testkey1.mypage_test.all())
-    test_app_key=TestApp.objects.get(id='5')
+    test_app_key=TestApp.objects.get(id='1')
     print(test_app_key.testname.all())
+    return render(request,'mypage/mypage.html')
+
+def mychallengeadd(request):
+    testkey1 = MyPage.objects.get(id='5')
+    test_app_key=TestApp.objects.get(id='5')
+    testkey1.mypage_test.add('4')
+    testkey1.mypage_test.remove('5')
     return render(request,'mypage/mypage.html')
