493896433d827d8ff3c1e3d129dcf16f61da3e19
jmpop <pjm970128@gmail.com>
2023-04-13 16:40:47 +0900

    erro fix

--- sikbbang_challenge_01/urls.py
+++ sikbbang_challenge_01/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('', include('qna.urls'))
+    path('', include('qna.urls')),
     path('',include('mypage.urls')),
     path('/api/challenge/', include('challenge.urls')),
 ]
