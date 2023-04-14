f75d805a9483c7023b5057323d645f66c2b5eee8
jmpop <pjm970128@gmail.com>
2023-04-14 14:30:37 +0900

    mypage-qna

--- mypage/models.py
+++ mypage/models.py
@@ -9,6 +9,15 @@ class MyPageModel(models.Model):
     user_key = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     qna_key=models.ManyToManyField(QnaModel,blank=True,related_name='mypage_key')
     challenge_key = models.ManyToManyField(ChallengeModel, blank=True, related_name='mypage_key')
-
+    try:
+        id=user_key.id
+    except:
+        id=0
     def __str__(self):
         return str(self.user_key.username)
+    def save(self, *args, **kwargs):
+        self.id=self.user_key.id
+        super().save(self, *args, **kwargs)
+        return
+
+
