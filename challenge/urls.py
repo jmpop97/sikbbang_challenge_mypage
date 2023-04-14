e06481e46e8b6cfa540e483d35fdafc413a3bc96
louis N <louis_n@naver.com>
2023-04-14 14:45:05 +0900

    중간 저장

--- challenge/urls.py
+++ challenge/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 app_name = 'challenge'
 
 urlpatterns = [
+    path('main/', views.view_main, name='challenge_detail'),
     path('challenge/posting/', views.view_posting_challenge,
          name='view_posting_challenge'),
     path('api/challenge/', views.posting_challenge, name='posting_challenge'),
     path('challenge/<int:id>', views.challenge_detail, name='challenge_detail'),
+
 ]
--- challenge/views.py
+++ challenge/views.py
@@ -6,24 +6,40 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+def view_main(request):
+    challenges = ChallengeModel.objects.all()
+    context = {
+        'challenges': challenges
+    }
+    return render(request, 'main.html', context)
+
+
+@login_required
 def view_posting_challenge(request):
     return render(request, 'challenge/posting.html')
 
 
+@login_required
 def posting_challenge(request):
     if request.method == 'POST':
+        challenge_author = request.user  # 현재 로그인한 유저
         challenge_title = request.POST.get('challenge_title')
         challenge_name = request.POST.get('challenge_name')
         challenge_genre = request.POST.get('challenge_genre')
         challenge_content = request.POST.get('challenge_content')
         challenge_image = request.FILES.get('challenge_image')
-        challenge = ChallengeModel(challenge_title=challenge_title, challenge_name=challenge_name,
-                                   challenge_genre=challenge_genre, challenge_content=challenge_content, challenge_image=challenge_image)
-        challenge.save()
+        if challenge_title and challenge_name and challenge_genre and challenge_content:
+            challenge = ChallengeModel(challenge_author=challenge_author, challenge_title=challenge_title, challenge_name=challenge_name,
+                                       challenge_genre=challenge_genre, challenge_content=challenge_content, challenge_image=challenge_image)
+            challenge.save()
 
-        return redirect('/challenge/posting')
+            return redirect('/challenge/posting')
+
+        else:
+            pass
 
 
+@login_required
 def challenge_detail(request, id):
     target_challenge = ChallengeModel.objects.get(id=id)
     if request.method == 'GET':
