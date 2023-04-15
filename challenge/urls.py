7a3a6af13a7add2a4fcc2898dd7d73ee3073f130
jmpop97 <99373073+jmpop97@users.noreply.github.com>
2023-04-15 19:58:24 +0900

    Update urls.py

--- challenge/urls.py
+++ challenge/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('challenge/posting/', views.view_posting_challenge,
          name='view_posting_challenge'),
     path('api/challenge/', views.posting_challenge, name='posting_challenge'),
-    path('challenge/<int:id>', views.challenge_detail, name='challenge_detail'),
-    path('challenge/<int:id>/delete',
+    path('challenge/<int:id>/', views.challenge_detail, name='challenge_detail'),
+    path('challenge/<int:id>/delete/',
          views.delete_challenge, name='delete_challenge'),
     path('search_results/', views.challenge_search_view, name='search-results'),
-    path('challenge/<int:id>/edit',
+    path('challenge/<int:id>/edit/',
          views.edit_challenge, name='edit_challenge'),
 ]
