28627829d403362db068abf7f90ad9759e2bb83d
jmpop <pjm970128@gmail.com>
2023-04-15 19:51:20 +0900

    bug fix for merge

--- challenge/urls.py
+++ challenge/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from . import views
 
-app_name = 'challenge'
+app_name = 'challenge_app'
 
 urlpatterns = [
     path('main/', views.view_main, name='view_main'),
     path('challenge/posting/', views.view_posting_challenge,
          name='view_posting_challenge'),
     path('api/challenge/', views.posting_challenge, name='posting_challenge'),
-    path('challenge/<int:id>/', views.challenge_detail, name='challenge_detail'),
-    path('challenge/<int:id>/delete/',
+    path('challenge/<int:id>', views.challenge_detail, name='challenge_detail'),
+    path('challenge/<int:id>/delete',
          views.delete_challenge, name='delete_challenge'),
     path('search_results/', views.challenge_search_view, name='search-results'),
-    path('challenge/<int:id>/edit/',
+    path('challenge/<int:id>/edit',
          views.edit_challenge, name='edit_challenge'),
 ]
