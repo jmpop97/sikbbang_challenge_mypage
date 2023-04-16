1d30a836da51c3585c8c7a9264ea2feaf5055f24
louis N <louis_n@naver.com>
2023-04-17 08:51:34 +0900

    시크릿키 재발급 및 보안 관리

--- sikbbang_challenge_01/settings.py
+++ sikbbang_challenge_01/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-__8l%xhk*=ze)_*&+e(y@nc5--h)o-kcj-gyy8p+7*8x%ec^50'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
